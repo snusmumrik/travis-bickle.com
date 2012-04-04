f24c252b605cd58b8d671c73f254a0203f93bf9e
Hiroyuki Kondo <hombredeestemundo@gmail.com>
2012-04-04 17:29:03 +0900

    modify migration: deleted_at for users

--- db/migrate/20120312053102_add_deleted_at_to_user.rb
+++ db/migrate/20120312053102_add_deleted_at_to_user.rb
@@ -1,5 +1,5 @@
 class AddDeletedAtToUser < ActiveRecord::Migration
   def change
-    add_column :users, :deleted_at, :datetime
+    add_column :users, :deleted_at, :datetime, :after => :confirmation_sent_at
   end
 end
