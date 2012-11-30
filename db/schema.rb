289ad17e5c8a2778e857205f2c61ed58c3c38dab
Hiroyuki Kondo <hombredeestemundo@gmail.com>
2012-11-30 14:49:43 +0900

    fixed replace breaks to rests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121104054941) do
+ActiveRecord::Schema.define(:version => 20121124063856) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -46,18 +46,6 @@ ActiveRecord::Schema.define(:version => 20121104054941) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
-  create_table "breaks", :force => true do |t|
-    t.integer  "report_id"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "ended_at"
-    t.datetime "deleted_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "breaks", ["report_id"], :name => "index_breaks_on_report_id"
-
   create_table "cars", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
@@ -65,9 +53,12 @@ ActiveRecord::Schema.define(:version => 20121104054941) do
     t.string   "car_model"
     t.integer  "base_fare"
     t.integer  "meter_fare"
+    t.integer  "twitter_id"
+    t.string   "twitter_name"
+    t.string   "access_token"
     t.datetime "deleted_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   add_index "cars", ["user_id"], :name => "index_cars_on_user_id"
@@ -136,14 +127,13 @@ ActiveRecord::Schema.define(:version => 20121104054941) do
   create_table "reports", :force => true do |t|
     t.integer  "driver_id"
     t.integer  "car_id"
+    t.integer  "meter"
     t.integer  "mileage"
     t.integer  "riding_mileage"
-    t.float    "riding_rate"
     t.integer  "riding_count"
     t.integer  "meter_fare_count"
     t.integer  "passengers"
     t.integer  "sales"
-    t.integer  "sales_par_kilometer"
     t.integer  "fuel_cost"
     t.integer  "ticket"
     t.integer  "account_receivable"
@@ -152,11 +142,25 @@ ActiveRecord::Schema.define(:version => 20121104054941) do
     t.integer  "deficiency_account"
     t.integer  "advance"
     t.datetime "deleted_at"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
   end
 
   add_index "reports", ["driver_id"], :name => "index_reports_on_driver_id"
+
+  create_table "rests", :force => true do |t|
+    t.integer  "report_id"
+    t.string   "location"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "started_at"
+    t.datetime "ended_at"
+    t.datetime "deleted_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "rests", ["report_id"], :name => "index_rests_on_report_id"
 
   create_table "rides", :force => true do |t|
     t.integer  "report_id"
