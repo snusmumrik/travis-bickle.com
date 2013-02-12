5060b30dd0c8c23fd971b674ce89d68ce42e57e6
Hiroyuki Kondo <hiroyukikondo@macbookair.lan>
2013-02-12 11:35:29 +0900

    fixed #16 car_cotroller before_filter fixed

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,5 @@
 class CarsController < InheritedResources::Base
-  before_filter :authenticate_user!, :except => :api_index
+  before_filter :authenticate_user!, :except => [:api_index, :api_update]
   before_filter :authenticate_owner, :only => [:show, :edit, :update]
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
