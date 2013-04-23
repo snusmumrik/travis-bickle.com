353c9d7986bc334853e93e477222a20b4cd116ca
Hiroyuki Kondo <hiroyukikondo@macbookair.lan>
2013-04-23 18:17:51 +0900

    #79 update devise2.0

--- db/migrate/20120312053102_add_deleted_at_to_user.rb
+++ db/migrate/20120312053102_add_deleted_at_to_user.rb
@@ -1,5 +1,5 @@
 class AddDeletedAtToUser < ActiveRecord::Migration
   def change
-    add_column :users, :deleted_at, :datetime, :after => :confirmation_sent_at
+    add_column :users, :deleted_at, :datetime, :after => :last_sign_in_at
   end
 end
