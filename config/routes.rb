56688bcd8009646d7c11a8273eb774b4ef9a679c
Hiroyuki Kondo <hiroyukikondo@macbookair.lan>
2013-05-23 12:27:41 +0900

    reactivate active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TravisBickle::Application.routes.draw do
+  root :to => 'locations#index'
+
   resources :documents
 
   resources :sales
@@ -55,7 +57,7 @@ TravisBickle::Application.routes.draw do
     end
   end
 
-  # ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
@@ -129,7 +131,7 @@ TravisBickle::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'locations#index'
+  # root :to => 'locations#index'
 
   # See how all your routes lay out with "rake routes"
 
