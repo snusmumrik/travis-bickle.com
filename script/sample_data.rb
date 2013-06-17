6f37bdb30cb0fd31b8b3f0f41caed68d62531abf
Hiroyuki Kondo <hiroyukikondo@macbookair.lan>
2013-06-17 16:49:22 +0900

    modify script/sample_data.rb

--- script/sample_data.rb
+++ script/sample_data.rb
@@ -1,14 +1,23 @@
+# -*- coding: utf-8 -*-
 # use under rails console
 
+@check_point_count = 10
 @driver_count = 20
 @car_count = 20
 
+@user = User.new(:username => "ユーザー#{User.count + 1}", :email => "test@test.com", :password => "password", :password_confirmation => "password")
+@user.save
+
+for i in 1..@check_point_count
+  CheckPoint.new(:user_id => @user.id, :name => "点検項目#{i}").save
+end
+
 for i in 1..@driver_count
-  Driver.new(:user_id => 1, :name => "driver#{format('%02d', i)}", :email => "driver#{i}@example.com", :password => "password").save
+  Driver.new(:user_id => @user.id, :name => "運転手#{format('%02d', i)}", :email => "driver#{i}@example.com", :password => "password").save
 end
 
 for i in 1..@car_count
-  Car.new(:user_id => 1, :name => "car#{format('%02d', i)}", :base_fare => 430, :meter_fare => 60).save
+  Car.new(:user_id => @user.id, :name => "#{format('%02d', i)}号車", :base_fare => 430, :meter_fare => 60).save
 end
 
 def create_records(year, month, day)
@@ -17,7 +26,7 @@ def create_records(year, month, day)
     report.save
 
     for j in 1..rand(19)+1
-      ride = Ride.new(:report_id => report.id, :ride_address => "ride#{j}", :leave_address => "leave#{j}", :passengers => rand(4) + 1, :fare => report.car.base_fare + report.car.meter_fare*(rand(9) + 1), :ended_at => DateTime.now)
+      ride = Ride.new(:report_id => report.id, :ride_address => "乗車地#{j}", :leave_address => "降車地#{j}", :passengers => rand(4) + 1, :fare => report.car.base_fare + report.car.meter_fare*(rand(9) + 1), :ended_at => DateTime.now)
       ride.save
       report.update_attributes(:riding_count => report.riding_count + 1,
                                :meter_fare_count => (ride.fare - report.car.base_fare) / report.car.meter_fare,
@@ -26,7 +35,7 @@ def create_records(year, month, day)
     end
 
     for k in 1..rand(4)+1
-      Rest.new(:report_id => report.id, :location => "location#{k}", :address => "adddress#{k}", :started_at => Time.now - 3*i*j*k, :ended_at => Time.now).save
+      Rest.new(:report_id => report.id, :location => "休憩地点#{k}", :address => "adddress#{k}", :started_at => Time.now - 30*i*j*k, :ended_at => Time.now).save
     end
 
     last_meter = Meter.includes(:report).where(["reports.car_id = ?", report.car_id]).last
@@ -47,8 +56,8 @@ def create_records(year, month, day)
                    :meter_fare_count => report.meter_fare_count)
     end
 
-    report.mileage = mileage
-    report.riding_mileage = (mileage * 0.95).ceil
+    report.mileage = (mileage * 0.95).ceil
+    report.riding_mileage = (mileage * 0.3).ceil
     report.account_receivable += 1/(rand(1000)+1)*100
     report.ticket = 1/(rand(1000)+1)*500
     report.fuel_cost = (report.sales*rand(5)/10.0).ceil
