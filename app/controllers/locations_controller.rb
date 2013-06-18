8420fabdf7b6bca279c4a09c60273c02f5bba416
Hiroyuki Kondo <hiroyukikondo@macbookair.lan>
2013-06-18 16:21:47 +0900

    #154 show only working cars

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -26,7 +26,7 @@ class LocationsController < InheritedResources::Base
   # GET /locations.json
   def index
     @title += "#{t('activerecord.models.location')}#{t('link.index')}"
-    @locations = Location.includes(:car => :user).where(["users.id = ?", current_user.id]).order("locations.car_id").all
+    @locations = Location.includes(:car => [:user, :reports]).where(["users.id = ? AND reports.finished_at IS NULL", current_user.id]).order("locations.car_id").all
     @json = @locations.to_gmaps4rails do |location, marker|
       begin
         if location.car.reports.where(["finished_at IS NULL"]).first.rides.where("leave_latitude IS NULL").first
