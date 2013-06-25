d7094ab73d9c2e40d87e6ff11082747f39d0527b
Hiroyuki Kondo <hiroyukikondo@macbookair.lan>
2013-06-25 14:24:04 +0900

    fixes balance culculation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -334,7 +334,7 @@ class ReportsController < InheritedResources::Base
   def check_balance
     credit = params[:report][:cash].to_i + params[:report][:ticket].to_i + params[:report][:account_receivable].to_i + params[:report][:fuel_cost].to_i + params[:report][:advance].to_i
     debit = params[:report][:sales].to_i + params[:report][:extra_sales].to_i
-    if debit - credit > 0
+    if debit - credit >= 0
       params[:report][:deficiency_account] = debit - credit
       params[:report][:surplus_funds] = 0
     elsif credit - debit > 0
