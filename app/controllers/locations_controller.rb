02d8740732d2be62b6824a920cb5723b3b3c42ee
Hiroyuki Kondo <hiroyukikondo@macbookair.lan>
2013-06-25 10:02:02 +0900

    fixes icon color bug fix

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -28,14 +28,14 @@ class LocationsController < InheritedResources::Base
     @title += "#{t('activerecord.models.location')}#{t('link.index')}"
     @locations = Location.includes(:car => [:user, :reports]).where(["users.id = ? AND reports.finished_at IS NULL", current_user.id]).order("locations.car_id").all
     @json = @locations.to_gmaps4rails do |location, marker|
-      # begin
-        if ride = location.car.reports.where("finished_at IS NULL").last.rides.last && ride.try(:leave_latitude).nil?
+      begin
+        if location.car.reports.where("finished_at IS NULL").last.rides.last && location.car.reports.where("finished_at IS NULL").last.rides.last.try(:leave_latitude).nil?
           marker.picture({
                            :picture => "http://chart.apis.google.com/chart?chst=d_map_spin&chld=1.1|0|FF0000|12|_|#{location.car.try(:name) }",
                            :width   => 100,
                            :height  => 100
                          })
-        elsif rest = location.car.reports.where("finished_at IS NULL").last.rests.last && rest.try(:ended_at).nil?
+        elsif location.car.reports.where("finished_at IS NULL").last.rests.last && location.car.reports.where("finished_at IS NULL").last.rests.last.try(:ended_at).nil?
           marker.picture({
                            :picture => "http://chart.apis.google.com/chart?chst=d_map_spin&chld=1.1|0|90ee90|12|_|#{location.car.try(:name) }",
                            :width   => 100,
@@ -48,13 +48,13 @@ class LocationsController < InheritedResources::Base
                            :height  => 100
                          })
         end
-      # rescue Exception => e
-      #   marker.picture({
-      #                    :picture => "http://chart.apis.google.com/chart?chst=d_map_spin&chld=1.1|0|FFFFFF|12|_|#{location.car.try(:name) }",
-      #                    :width   => 100,
-      #                    :height  => 100
-      #                  })
-      # end
+      rescue Exception => e
+        marker.picture({
+                         :picture => "http://chart.apis.google.com/chart?chst=d_map_spin&chld=1.1|0|FFFFFF|12|_|#{location.car.try(:name) }",
+                         :width   => 100,
+                         :height  => 100
+                       })
+      end
 
       marker.infowindow "<img src=\"http://static.tumblr.com/gw0wbvo/y2Emn54fm/cars_photo02.png\" width=200 height=200><br /><a href='/notifications/new?car_id=#{location.car_id}'>#{location.car.try(:name)}へ配車指示</a><br /><a href=\"/reports/#{location.car.reports.last.id}\">乗務記録</a>"
       marker.title location.car.try(:name)
