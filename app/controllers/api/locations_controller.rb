87c396553f8e6bb662c4cdff1ff41899a59fb8cf
Hiroyuki Kondo <hiroyuki@chishaku.com>
2014-04-03 14:29:32 +1300

    #349 api for call travis

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,6 +1,25 @@
+# -*- coding: utf-8 -*-
 class Api::LocationsController < ApplicationController
-  before_filter :authenticate_token
+  before_filter :authenticate_devise, :only => :index
+  before_filter :authenticate_token, :only => :update
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+
+  # called from call travis
+  # GET /locations
+  # GET /locations.json
+  def index
+    @locations = Location.includes(:car => :user).near([params[:latitude], params[:longitude]], 10)
+    @cars = Hash.new
+    @users = Hash.new
+    @locations.each do |location|
+      @cars.store(location.id, location.car)
+      @users.store(location.id, location.car.user)
+    end
+
+    respond_to do |format|
+      format.json { render json: {:locations => @locations, :cars => @cars, :users => @users} }
+    end
+  end
 
   # PUT /api/locations/1
   # PUT /api/locations/1.json
@@ -21,6 +40,10 @@ class Api::LocationsController < ApplicationController
   end
 
   private
+  def authenticate_devise
+    render json:{ :error => "Not Acceptable:locations#index", :status => 406 } unless params[:latitude] && params[:longitude] # && params[:device_token]
+  end
+
   def authenticate_token
     # params[:id] == car_id  !!caution!!
     @car = Car.where(["id = ? AND device_token = ? AND deleted_at IS NULL", params[:id], params[:device_token]]).first
