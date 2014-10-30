ef8e2b922aaecdd65b37943878dd52912a799410
Hiroyuki Kondo <hiroyuki@chishaku.com>
2014-10-30 12:00:41 +0900

    change signin redirect path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
     @description = @@description
     @keywords = @@keywords
   end
+
+  def after_sign_in_path_for(resource)
+    "#{reports_path}/#{Date.today.year}/#{Date.today.month}/#{Date.today.day}"
+  end
 end
