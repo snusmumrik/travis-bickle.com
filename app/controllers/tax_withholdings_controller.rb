82b725064a940e614ac6b75a657c55771d76fa71
Hiroyuki Kondo <hiroyuki@chishaku.com>
2015-07-30 16:29:12 +0900

    #603 add valication to tax_withholding

--- app/controllers/tax_withholdings_controller.rb
+++ app/controllers/tax_withholdings_controller.rb
@@ -5,6 +5,42 @@ class TaxWithholdingsController < InheritedResources::Base
   before_filter :authenticate_owner, :except => [:index, :new, :create]
   before_filter :prepare_options, :only => [:new, :create, :edit, :update]
 
+  # GET /tax_withholdings/:driver_id/:year
+  # GET /tax_withholdings/:driver_id/:year.json
+  def index
+    title = "所得税源泉徴収簿 #{@reports[0].started_at.strftime('%Y%0m')}_(#{@reports[0].driver.name})" rescue "所得税源泉徴収簿"
+
+    @driver = Driver.find(params[:driver_id])
+
+    @tax_withholdings = TaxWithholding.where(["driver_id = ? AND date BETWEEN ? AND ?",
+                                             params[:driver_id],
+                                             Date.new(params[:year].to_i, 1, 1),
+                                             Date.new(params[:year].to_i, 12, 31)
+                                            ])
+    @tax_withholding_hash = Hash.new
+    @salary_subtotals = Hash.new
+    @social_insurence_subtotals = Hash.new
+    @total_hash = Hash.new do |hash, key|
+      hash[key] = 0
+    end
+
+    @tax_withholdings.each do |t|
+      @tax_withholding_hash.store(t.date.month, t)
+      @salary_subtotals.store(t.date.month, t.base_salary + t.percentage_pay + t.holiday_pay + t.night_pay + t.extra_pay + t.no_absence_pay + t.no_accident_pay + t.long_service_pay + t.real_salary)
+      @social_insurence_subtotals.store(t.date.month, t.health_insurance + t.nursing_insurance + t.pension + t.unemployment_insurance)
+      @total_hash[:working_days] += t.working_days
+    end
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tax_withholding_hash }
+      format.pdf do
+        # render :pdf => title, :orientation => "Landscape", :encoding => "UTF-8"
+        render :pdf => title, :encoding => "UTF-8"
+      end
+    end
+  end
+
   # GET /tax_withholdings/new/:driver_id/:year/:month
   # GET /tax_withholdings/new/:driver_id/:year/:month.json
   def new
@@ -63,42 +99,6 @@ class TaxWithholdingsController < InheritedResources::Base
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @tax_withholding }
-    end
-  end
-
-  # GET /tax_withholdings/:driver_id/:year
-  # GET /tax_withholdings/:driver_id/:year.json
-  def index
-    title = "所得税源泉徴収簿 #{@reports[0].started_at.strftime('%Y%0m')}_(#{@reports[0].driver.name})" rescue "所得税源泉徴収簿"
-
-    @driver = Driver.find(params[:driver_id])
-
-    @tax_withholdings = TaxWithholding.where(["driver_id = ? AND date BETWEEN ? AND ?",
-                                             params[:driver_id],
-                                             Date.new(params[:year].to_i, 1, 1),
-                                             Date.new(params[:year].to_i, 12, 31)
-                                            ])
-    @tax_withholding_hash = Hash.new
-    @salary_subtotals = Hash.new
-    @social_insurence_subtotals = Hash.new
-    @total_hash = Hash.new do |hash, key|
-      hash[key] = 0
-    end
-
-    @tax_withholdings.each do |t|
-      @tax_withholding_hash.store(t.date.month, t)
-      @salary_subtotals.store(t.date.month, t.base_salary + t.percentage_pay + t.holiday_pay + t.night_pay + t.extra_pay + t.no_absence_pay + t.no_accident_pay + t.long_service_pay + t.real_salary)
-      @social_insurence_subtotals.store(t.date.month, t.health_insurance + t.nursing_insurance + t.pension + t.unemployment_insurance)
-      @total_hash[:working_days] += t.working_days
-    end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @tax_withholding_hash }
-      format.pdf do
-        # render :pdf => title, :orientation => "Landscape", :encoding => "UTF-8"
-        render :pdf => title, :encoding => "UTF-8"
-      end
     end
   end
 
