cbe5e2847d30641c74dfbb859709b1c2c69f6829
Hiroyuki Kondo <hiroyuki@chishaku.com>
2015-11-24 14:25:54 +0900

    revert 52620c6d06237634cada77bc7c3184cf1b278bac

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TravisBickle::Application.config.secret_token = '97807e406ca613dc16f7750f940c8c88d06464d0f16c5cee27943455756343e9b7d4ac5364d684422e3c393bbbaee8fafbe7535e0c93cc1b7674e584c27c7813'
